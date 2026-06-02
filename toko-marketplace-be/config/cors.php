e6569e5a2e383847ec67b26849134a0f6cb9d9a1
olissaid <cholis.cholis7788@gmail.com>
2026-06-02 09:40:05 +0700

    fix: update cors allowed origins for vercel

---
+++ toko-marketplace-be/config/cors.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    // Daftarkan localhost dan link Vercel temen lo di sini
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://toko-marketplace-fe.vercel.app',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    // Diubah jadi true agar Frontend bisa kirim cookie/token login dengan aman
+    'supports_credentials' => true,
+
+];
